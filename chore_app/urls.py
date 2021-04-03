4c75874f0dff4633631deebbff929568d28147dc
Jose_G <josealegomez1996@gmail.com>
2021-04-03 00:30:41 -0400

    added user_dash.html and updated index

--- chore_app/urls.py
+++ chore_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name ='home'),
+    path('user_dash', views.user_dash)
 ]
--- chore_app/views.py
+++ chore_app/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+
+def user_dash(request):
+    return render(request, 'user_dash.html')
