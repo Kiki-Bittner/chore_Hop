68f004db257633b68af3bf999a03b2e2c6178e67
Roman Dzadzic <roman.dzadzic@gmail.com>
2021-04-05 16:07:18 -0500

    Releasing changes to views.py, urls.py and index.html

--- chore_app/urls.py
+++ chore_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.index, name ='home'),
     path('landing/', ChoreLandingPageView.as_view(), name = 'landing-page'),
     path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path("register", views.register),
+    path("login", views.login),
 ]
